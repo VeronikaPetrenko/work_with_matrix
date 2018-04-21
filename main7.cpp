3cbf9235093854ac070f652840eb91c2a483810b
Вероника Петренко <37023778+VeronikaPetrenko@users.noreply.github.com>
2018-04-21 21:24:11 +0300

    Create main7.cpp

---
+++ main7.cpp
@@ -0,0 +1,43 @@
+//Дана квадратная матртца размером n,нужно поменять местами её главную и побочную диагонали.
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+	setlocale(LC_ALL,"Russian");
+	int n,m,i,j;
+	cout<<"Введите размер матрицы:  ";
+	cin>>n;
+	if(n<=0){								            //защита от дурака
+		cout<<"Ошибка!"<<endl;
+		system("pause");
+		return 0;
+	}
+	cout<<endl;
+	int **arr=new int *[n];					    //создаём квадратную матрицу из рандомных чисел 
+	for(i=0;i<n;i++)
+	arr[i]=new int [n];
+	for(int i=0; i<n;i++)
+		for( j=0;j<n;j++){
+			arr[i][j]=((rand()%100));}
+	cout<<"Ваша матрица: "<<endl;			  //выводим матрицу
+	for( i=0; i<n;i++){
+		for( j=0;j<n;j++){
+			if(arr[i][j]<10){			        	//для красоты выводимости матрицы
+				cout<<" ";}
+			cout<<arr[i][j]<<" ";}
+		cout<<endl;}
+	cout<<endl;
+	for( i=0; i<n;i++){
+		for( j=0;j<n;j++){
+			if(j==i){
+			swap(arr[i][j],arr[i][n-j-1]);}}
+	}
+	cout<<"Изменённая матрица : "<<endl;
+	for( i=0; i<n;i++){
+		for( j=0;j<n;j++){
+			if(arr[i][j]<10){				         //для красоты выводимости матрицы
+				cout<<" ";}
+			cout<<arr[i][j]<<" ";}
+	cout<<endl;
+	}
+}
